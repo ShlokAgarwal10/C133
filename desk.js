9c285ed207eef2d9cb8beb50abba9da1aa0a4e02
ShlokAgarwal10 <76897077+ShlokAgarwal10@users.noreply.github.com>
2021-07-29 20:25:14 +0530

    Add files via upload

---
+++ desk.js
@@ -0,0 +1,45 @@
+status = "";
+img = "";
+objects=[];
+
+function desk_home(){
+    window.location = "index.html";
+}
+function preload(){
+    img = loadImage("desk.jpeg")
+    }
+function setup(){
+    canvas = createCanvas(640,420);
+    canvas.center();
+    
+    objectDetector = ml5.objectDetector('cocossd',modelLoaded);
+    document.getElementById("status").innerHTML="status: detecting objects";
+    }
+    function modelLoaded(){
+        console.log("model loaded");
+        status = true;
+        objectDetector.detect(img,gotResults);
+    }
+    function gotResults(error,results){
+        if(error){
+            console.log(error);
+        }
+        console.log(results);
+        objects = results;
+        }
+        function draw(){
+            image(img,0,0,640,420);
+
+            if(status != ""){
+                for(i = 0;i < objects.length;i++){
+                    document.getElementById("status").innerHTML = "status : object detected";
+                    fill("#FF0000");
+                    percent = floor(objects[i].confidence*100);
+                    text(objects[i].label+" "+percent+"%",objects[i].x,objects[i].y);
+                    noFill();
+                    stroke("#FF0000");
+                    rect(objects[i].x,objects[i].y,objects[i].width,objects[i].height);
+                }
+                }
+                
+        }
---
+++ main.js
@@ -0,0 +1,12 @@
+function home_desk(){
+    window.location = "desk.html";
+}
+function home_bed(){
+    window.location = "bed.html";
+}
+function home_balcony(){
+    window.location = "balcony.html";
+}
+function home_table(){
+    window.location = "table.html";
+}
